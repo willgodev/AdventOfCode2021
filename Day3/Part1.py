de70ce8951a3ba9a33b1f9106edd8e434ac9b392
wgoins6 <wgoins6@gatech.edu>
2022-02-28 21:38:41 -0500

    Completed Day3-Part1

---
+++ Day3/Part1.py
@@ -0,0 +1,25 @@
+if __name__ == "__main__":
+    with open("input", "r") as input_file:
+        nums = [num.strip() for num in input_file]
+        occurrence_list = []
+        for num in nums[0]:
+            occurrence_list.append(0)
+        for num in nums:
+            for i in range(len(num)):
+                if num[i] == '0':
+                    occurrence_list[i] -= 1
+                else:
+                    occurrence_list[i] += 1
+        gamma_rate = ''
+        mask = ''
+        for occurrence in occurrence_list:
+            if occurrence >= 0:
+                gamma_rate += '1'
+            else:
+                gamma_rate += '0'
+            mask += '1'
+        epsilon_rate = ''.join('1' if c == '0' else '0' for c in gamma_rate)
+        gamma_rate = int(gamma_rate, 2)
+        epsilon_rate = int(epsilon_rate, 2)
+        answer = gamma_rate * epsilon_rate
+        print(answer)
